5f91bccd800d978fa77ff7c6c64c088ffed15034
txy-nju <2431116525@qq.com>
2025-07-04 16:17:37 +0800

    初始化项目，构建前端的项目框架

---
+++ back/src/config/config.default.ts
@@ -0,0 +1,9 @@
+import { MidwayConfig } from '@midwayjs/core';
+
+export default {
+  // use for cookie sign key, should change to your own and keep security
+  keys: '1751614576613_4517',
+  koa: {
+    port: 7001,
+  },
+} as MidwayConfig;
